9f03aac6a6fba8c52733cd7f79fed64d1915cc55
Baebadoobee <hortock@outlook.com>
2024-12-19 21:10:50 -0300

    Screen display update

--- files.bkp/config.bkp/qtile/autostart.sh
+++ files.bkp/config.bkp/qtile/autostart.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 
-EXTERNAL_OUTPUT="HDMI1" #sua saída externa
-INTERNAL_OUTPUT="LVDS1" #sua tela 
+#EXTERNAL_OUTPUT="HDMI1" #sua saída externa
+#INTERNAL_OUTPUT="LVDS1" #sua tela 
 
-xrandr | grep $EXTERNAL_OUTPUT | grep " connected "
-if [ $? -eq 0 ]; then
-    xrandr --output $INTERNAL_OUTPUT --off --output $EXTERNAL_OUTPUT --auto 
-else
-    xrandr --output $INTERNAL_OUTPUT --auto --output $EXTERNAL_OUTPUT --off
-fi
+#xrandr | grep $EXTERNAL_OUTPUT | grep " connected "
+#if [ $? -eq 0 ]; then
+#    xrandr --output $INTERNAL_OUTPUT --off --output $EXTERNAL_OUTPUT --auto 
+#else
+#    xrandr --output $INTERNAL_OUTPUT --auto --output $EXTERNAL_OUTPUT --off
+#fi
 
 xset -b
-# xrandr --output LVDS1 --mode 1366x768
+xrandr --output LVDS1 --mode 1366x768
 # xrandr --output HDMI1 --mode 1360x768
 nitrogen --restore &
 polybar &
